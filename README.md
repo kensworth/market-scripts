057cdc902018876d2fe32ea8268533b160b2a234
Ken Zhang <kyz211@nyu.edu>
2017-01-07 01:27:14 +0800

    add async functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+$ python market_query.py // make sure this is python3 and grequests is installed
--- market_query.py
+++ market_query.py
@@ -1,7 +1,6 @@
-import requests
-import json
+import grequests
 
-def query_stock(symbol_list):
+def construct_url(symbol_list):
     url = 'https://query.yahooapis.com/v1/public/yql';
     start_date = '2017-01-04'
     end_date = '2017-01-05'
@@ -9,12 +8,9 @@ def query_stock(symbol_list):
         symbol_list + ') and startDate="' + start_date + '" and endDate="' + \
         end_date + '"&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json'
     constructed_url = url + '?q=' + data
+    return constructed_url
 
-    response = requests.get(constructed_url)
-    print constructed_url
-    print response.text
-
-def iterate_stocks():
+def batch_stocks():
     f = open('nasdaqlisted.txt')
     lines = f.readlines()[1:-1]
     tickers = []
@@ -26,8 +22,19 @@ def iterate_stocks():
         if i % 100 == 0:
             batches.append([])
         batches[-1].append(tickers[i])
+    return batches
+
+def driver():
+    batches = batch_stocks()
+    urls = []
     for batch in batches:
         symbol_list = ', '.join('"{0}"'.format(ticker) for ticker in batch)
-        query_stock(symbol_list)
+        constructed_url = construct_url(symbol_list)
+        urls.append(constructed_url)
+    rs = [grequests.get(url) for url in urls]
+    responses = grequests.map(rs, size=5)
+    for response in responses:
+        print(response.text)
 
-iterate_stocks()
+if __name__ == "__main__":
+    driver()
