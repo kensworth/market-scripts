fbf5be7fa1bb9aedd0e4e1be422946e0274a59c3
Ken Zhang <kyz211@nyu.edu>
2017-01-07 00:45:20 +0800

    enable batch requests of 100

--- market_query.py
+++ market_query.py
@@ -1,24 +1,33 @@
 import requests
 import json
 
-def query_stock():
+def query_stock(symbol_list):
     url = 'https://query.yahooapis.com/v1/public/yql';
-    start_date = '2017-01-01'
-    end_date = '2017-01-06'
-    symbol_list = '"GOOG"'
+    start_date = '2017-01-04'
+    end_date = '2017-01-05'
     data = 'select * from yahoo.finance.historicaldata where symbol in (' + \
         symbol_list + ') and startDate="' + start_date + '" and endDate="' + \
         end_date + '"&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json'
     constructed_url = url + '?q=' + data
 
     response = requests.get(constructed_url)
+    print constructed_url
     print response.text
 
 def iterate_stocks():
     f = open('nasdaqlisted.txt')
     lines = f.readlines()[1:-1]
+    tickers = []
+    batches = []
     for line in lines:
         ticker = line.split('|')[0]
-        print ticker
+        tickers.append(ticker)
+    for i in range(len(tickers)):
+        if i % 100 == 0:
+            batches.append([])
+        batches[-1].append(tickers[i])
+    for batch in batches:
+        symbol_list = ', '.join('"{0}"'.format(ticker) for ticker in batch)
+        query_stock(symbol_list)
 
 iterate_stocks()
